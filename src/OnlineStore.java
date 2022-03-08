6b4f2e630be1e26fb63d198326e979f8383395a3
d4nib <dblancoalb@ouc.edu>
2022-03-08 22:58:54 +0100

    Creación de clases 3

--- src/OnlineStore.java
+++ src/OnlineStore.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
+import OScontroller.CustomerController;
+import OScontroller.OrderController;
+import OScontroller.ProductController;
+
 public class OnlineStore {
+    private ArrayList<OSmodel.Order> orders;
+    private ArrayList<OSmodel.Customer> customers;
+    private ArrayList<OSmodel.Product> products;
+    private OrderController orderController;
+    private CustomerController customerController;
+    private ProductController productController;
+
+    
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
